bc0b837df1d1e8d53e790cf79b5d26c4875ff064
smorloc <smorloc@gmail.com>
2013-01-14 18:12:01 -0500

    Initial commit

---
+++ ChangeFilamentAtZ.py
@@ -0,0 +1,58 @@
+#Name: Pause at Z for filament change
+#Info: Pause the print at a certain height, move to a specified parking location, disable extruder stepper
+#Depend: GCode
+#Type: postprocess
+#Param: pauseZ(float:5.0) Pause Z height (mm)
+#Param: parkX(float:190) Head park X (mm)
+#Param: parkY(float:190) Head park Y (mm)
+#Param: parkZ(float:190) Head park Z (mm)
+#Param: retractAmount(float:5) Retraction amount (mm)
+
+import re
+
+def getValue(line, key, default = None):
+	if not key in line or (';' in line and line.find(key) > line.find(';')):
+		return default
+	subPart = line[line.find(key) + 1:]
+	m = re.search('^[0-9]+\.?[0-9]*', subPart)
+	if m == None:
+		return default
+	try:
+		return float(m.group(0))
+	except:
+		return default
+
+with open(filename, "r") as f:
+	lines = f.readlines()
+
+z = 0
+x = 0
+y = 0
+pauseState = 0
+with open(filename, "w") as f:
+	for line in lines:
+		if getValue(line, 'G', None) == 1:
+			newZ = getValue(line, 'Z', z)
+			x = getValue(line, 'X', x)
+			y = getValue(line, 'Y', y)
+			if newZ != z:
+				z = newZ
+				if z < pauseZ and pauseState == 0:
+					pauseState = 1
+				if z >= pauseZ and pauseState == 1:
+					pauseState = 2
+					# Retract
+					f.write("M83\n")		# Set E codes relative while in Absolute Coordinates (G90) 
+					f.write("G1 E-%f F6000\n" % (retractAmount))
+					# Move the head to specified location
+					f.write("G1 X%f Y%f Z%f F9000\n" % (parkX, parkY, parkZ))
+					# Disable extruder stepper
+					f.write("M84 E\n")					
+					# Wait until the user continues printing
+					f.write("M0\n")
+					# Move the head back to printing location
+					f.write("G1 X%f Y%f F9000\n" % (x, y))
+					f.write("G1 E0 F6000\n")
+					f.write("G1 F9000\n")
+					f.write("M82\n")		# Set E codes absolute (default) 
+		f.write(line)
